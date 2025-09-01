c4b76011e5642ec5a51a8dd925078dba22ae3b64
TunjiPaul <ogorpaul877@gmail.com>
2025-09-01 15:16:14 +0100

    mock

---
+++ Python_projects/to_d0_list.py
@@ -0,0 +1,68 @@
+#functions to collect basic operations for the to-do-list.
+
+#function to add tasks.
+# List to store tasks
+tasks = []
+marked_tasks = []
+# Function to add a task to the list
+def add_task(task):
+    tasks.append(task)
+    print(f'Task "{task}" added!')
+
+# Function to remove a task from the list
+def remove_task(task):
+    if task in tasks:
+        tasks.remove(task)
+        print(f'Task "{task}" removed!')
+    else:
+        print("Task not found!")
+
+# Function to display all tasks
+def view_tasks():
+    if tasks:
+        print("Your Tasks:")
+        for idx, task in enumerate(tasks, 1):  # Start numbering from 1
+            print(f'{idx}. {task}')
+    else:
+        print("No tasks in your list!")
+
+     
+#function to add completed tasks to the list.
+def completed_tasks(marked_task):
+    if tasks:
+     marked_tasks.append(marked_task)
+     print(f"Task {marked_task} completed.")
+     print(marked_tasks)
+     
+
+# def edit_tasks(task):
+#     if tasks:
+#         return tasks.append(task)
+# Infinite loop to keep the program running until user exits
+while True:
+    print("\nOptions: 1. Add Task  2. Remove Task  3. View Tasks  4. Exit 5. Completed Tasks 6. Edit Tasks")
+    try:
+    # Ask user for their choice
+     choice = input("Enter your choice: ").strip()
+    except:
+     print("W")
+    # if task.lower():
+    # for i in add_task:
+     if choice == '1' or "one":
+        task = input("Enter task: ")
+        add_task(task)
+     elif choice == '2':
+        task = input("Enter task to remove: ")
+        remove_task(task)
+     elif choice == '3':
+        view_tasks()
+    
+     elif choice == '4':
+        print("Exiting program. Have a productive day!")
+        break
+     elif choice == '5':
+        input("Add completed tasks: ")
+        completed_tasks(marked_tasks)
+        
+     else:
+        print("Invalid choice! Please select a valid option.")
---
+++ rough.py
@@ -0,0 +1,4 @@
+a =33
+b = 33
+if b > a:
+    print("greater")
