c51859326ae07ca0598fc55885d8aefa5fde5d4f
your name <ogorpaul877@gmail.com>
2025-09-04 09:06:00 +0100

    commit update

--- Python_projects/temperature_converter.py
+++ Python_projects/temperature_converter.py
@@ -1,65 +1,79 @@
-#define functions for all temperature parameters.
+# define functions for all temperature parameters.
 
 def celsius_to_kelvin(c):
     while True:
-     try:
-        if c < 0:
-         raise ValueError("Kelvin is absolute and cannot be less than zero")
-        else:
-            return c + 273
-     except ValueError as e:
-        print("error:", e)
-        break
+        try:
+            if c < 0:
+                raise ValueError(
+                    "Kelvin is absolute and cannot be less than zero")
+            else:
+                return round(c + 273, 2)
+        except ValueError as e:
+            print("error:", e)
+            break
+
+
 def celsius_to_farenheit(c):
-    return  (c * 1.8) + 32
+    return round((c * 1.8) + 32, 2)
+
+
 def kelvin_to_celsius(k):
-    return k-273
+    return round(k-273, 2)
+
+
 def kelvin_to_farenheit(k):
-    return (1.8 * k) - 459.67
+    return round((1.8 * k) - 459.67, 2)
+
+
 def farenheit_to_kelvin(f):
- while True:
-    try:
-        if f < 0:
-            raise ValueError("Kelvin is absolute and cannot be less than zero.")
-        else:
-            return (f + 459.67)/1.8
-    except ValueError as e:
-        print("error: ", e) 
-    break
+    while True:
+        try:
+            if f < 0:
+                raise ValueError(
+                    "Kelvin is absolute and cannot be less than zero.")
+            else:
+                return round((f + 459.67)/1.8, 2)
+        except ValueError as e:
+            print("error: ", e)
+        break
 
 
 def farenheit_to_celsius(f):
-    return (f-32)/1.8
+    return round((f-32)/1.8, 2)
 
-#collect input for temp, choice and validate with try and except.
+
+# collect input for temp, choice and validate with try and except.
 name = input("what is your name: ")
 print(f"Welcome to {name} temp. converter")
 
 
 while True:
     try:
-     print("Convert from:\n1. Celsius to Farenheit\n2. Celsius to Kelvin\n3. Farenheit to Celsius\n4. Farenheit to Kelvin\
+        print("Convert from:\n1. Celsius to Farenheit\n2. Celsius to Kelvin\n3. Farenheit to Celsius\n4. Farenheit to Kelvin\
       \n5. Kelvin to Celsius\n6. Kelvin to Farenheit\n7. Exit")
-     choice = input("Pick from the following options 1/2/3/4/5/6/7: ")
-     temp = float(input("Enter the value of the temperature: "))
-     if choice == "1":
-          print(f"Celsius -> Farenheit: {celsius_to_farenheit(temp):.2f} farenheit")  
-     elif choice == "2":
-         print(f"Celsius -> Kelvin: {celsius_to_kelvin(round(temp,2))} kelvin")
-     elif choice == "3":
-         print(f"Farenheit -> Celsius: {farenheit_to_celsius(round(temp,2))} celsius")
-     elif choice == "4":
-         print(f"Farenheit -> Kelvin: {farenheit_to_kelvin(temp):.2f} kelvin")
-     elif choice == "5":
-         print(f"Kelvin -> Celsius: {kelvin_to_celsius(round(temp,2))} celsius")
-     elif choice == "6":
-         print(f"Kelvin -> Farenheit: {kelvin_to_farenheit(temp):.2f} farenheit")
-     elif choice == "7":
-         print(f"Thank you for using {name} converter! Have a productive day!")
-         break
-     else:
-         print("You have picked the wrong option. Try Again.")
+        choice = input("Pick from the following options 1/2/3/4/5/6/7: ")
+        temp = float(input("Enter the value of the temperature: "))
+        if choice == "1":
+            print(
+                f"Celsius -> Farenheit: {celsius_to_farenheit(temp)} farenheit")
+        elif choice == "2":
+            print(f"Celsius -> Kelvin: {celsius_to_kelvin(temp)} kelvin")
+        elif choice == "3":
+            print(
+                f"Farenheit -> Celsius: {farenheit_to_celsius(temp)} celsius")
+        elif choice == "4":
+            print(f"Farenheit -> Kelvin: {farenheit_to_kelvin(temp)} kelvin")
+        elif choice == "5":
+            print(f"Kelvin -> Celsius: {kelvin_to_celsius(temp)} celsius")
+        elif choice == "6":
+            print(
+                f"Kelvin -> Farenheit: {kelvin_to_farenheit(temp)} farenheit")
+        elif choice == "7":
+            print(
+                f"Thank you for using {name} converter! Have a productive day!")
+            break
+        else:
+            print("You have picked the wrong option. Try Again.")
     except ValueError:
-        raise("You have entered the wrong value for temperature")
-#handle edge cases such as negative kelvin values.
-    
+        raise ("You have entered the wrong value for temperature")
+# handle edge cases such as negative kelvin values.
--- Python_projects/to_d0_list.py
+++ Python_projects/to_d0_list.py
@@ -1,15 +1,19 @@
-#functions to collect basic operations for the to-do-list.
+# functions to collect basic operations for the to-do-list.
 
-#function to add tasks.
+# function to add tasks.
 # List to store tasks
 tasks = []
 marked_tasks = []
 # Function to add a task to the list
+
+
 def add_task(task):
     tasks.append(task)
     print(f'Task "{task}" added!')
 
 # Function to remove a task from the list
+
+
 def remove_task(task):
     if task in tasks:
         tasks.remove(task)
@@ -18,6 +22,8 @@ def remove_task(task):
         print("Task not found!")
 
 # Function to display all tasks
+
+
 def view_tasks():
     if tasks:
         print("Your Tasks:")
@@ -26,8 +32,8 @@ def view_tasks():
     else:
         print("No tasks in your list!")
 
-     
-#function to add completed tasks to the list.
+
+# function to add completed tasks to the list.
 def complete_task(task_to_mark):
     global tasks
     if task_to_mark in tasks:
@@ -37,51 +43,55 @@ def complete_task(task_to_mark):
         print(f"Task ({task_to_mark}) completed.")
     else:
         print("This task does not exist. Have you created it before?")
+# function to edit tasks
+
 
 def edited_tasks(task_to_edit):
     if task_to_edit in tasks:
-# print((tasks.index(task_to_edit)))
-            tasks.insert(tasks.index(task_to_edit), replacement)
-            tasks.remove(task_to_edit)
+        # print((tasks.index(task_to_edit)))
+        tasks.insert(tasks.index(task_to_edit), replacement)
+        tasks.remove(task_to_edit)
 
-            print(f"Task ({task_to_edit}) has been changed to ({replacement})")
-            view_tasks()
-    else: 
-            print("Task doesn't exist.")
+        print(f"Task ({task_to_edit}) has been changed to ({replacement})")
+        view_tasks()
+    else:
+        print("Task doesn't exist.")
+
 
 # Infinite loop to keep the program running until user exits
 while True:
     print("\nOptions: 1. Add Task  2. Remove Task  3. View Tasks  4. Exit 5. Completed Tasks 6. Edit Tasks")
-    # try:
-    # Ask user for their choice
-    choice = input("Enter your choice: ").strip()
-    if choice == '1':
-        task = input("Enter task: ")
-        add_task(task)
-    elif choice == '2':
-        task = input("Enter task to remove: ")
-        remove_task(task)
-    elif choice == '3':
-        view_tasks()
-    
-    elif choice == '4':
-        print("Exiting program. Have a productive day!")
-        break
-    elif choice == '5':
-     task_to_mark =   input("Add completed tasks: ")
-     complete_task(task_to_mark)
-    elif choice == '6':
-        view_tasks()
-        task_to_edit = input("Enter the name of the task you want to edit: ").strip()
-        replacement = input("Enter new task: ").strip()
-        edited_tasks(task_to_edit)
-          
-        
-    else:
-        print("Invalid choice! Please select a valid option.")
+    try:
+        # Ask user for their choice
+        choice = input("Enter your choice: ").strip()
+        if choice == '1':
+            task = input("Enter task: ")
+            add_task(task)
+        elif choice == '2':
+            task = input("Enter task to remove: ")
+            remove_task(task)
+        elif choice == '3':
+            view_tasks()
 
-    print("Completed Tasks:", marked_tasks)
+        elif choice == '4':
+            print("Exiting program. Have a productive day!")
+            break
+        elif choice == '5':
+            task_to_mark = input("Add completed tasks: ")
+            complete_task(task_to_mark)
+        elif choice == '6':
+            view_tasks()
+            task_to_edit = input("Enter the name of the task you want to edit: ").strip()
+            replacement = input("Enter new task: ").strip()
+            edited_tasks(task_to_edit)
+        else:
+            print("Invalid choice! Please select a valid option.")
 
-    with open("schedule.txt", "w", encoding= "utf-8") as f:
-        f.write(f"These are the printed tasks: {str(tasks)}\n")
-        f.write(f"These are the completed tasks: {str(marked_tasks)}")
+        print("Completed Tasks:", marked_tasks)
+
+        with open("schedule.txt", "w", encoding="utf-8") as f:
+            f.write(f"These are the printed tasks: {str(tasks)}\n")
+            f.write(f"These are the completed tasks: {str(marked_tasks)}")
+    except ValueError:
+     raise ("You have inputed the wrong value")
+
